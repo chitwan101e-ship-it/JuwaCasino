3e5cae2c7e5cd44c54596b58fd83cc2eed2b6e9c
chitwan101e-ship-it <chitwan101e@gmail.com>
2025-10-10 02:51:13 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-export default defineConfig({ plugins:[react()], resolve:{ alias:{ '@': path.resolve(__dirname,'./src') } } })
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  base: '/', // Change this to '/JuwaCasino/' if your repo is not using a custom domain
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
+})
